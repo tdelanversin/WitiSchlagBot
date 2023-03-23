4f042948604cb72d7aebfa3a9eeda81f9601be27
Tilman de Lanversin <tilmand@student.ethz.ch>
2023-03-23 16:06:32 +0100

    ✨ make it work!

--- bot.py
+++ bot.py
@@ -1,80 +1,193 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument, wrong-import-position
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
+from queue import Queue
+from telegram import (
+    Update,
+    Message,
+)
+from telegram.ext import (
+    filters,
+    MessageHandler,
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+)
 
-from telegram import __version__ as TG_VER
+from transformers import pipeline  # type: ignore
 
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+summarizer = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
 
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    raise RuntimeError(f"PTB version (version {TG_VER}) outdated. Please update to >= 20.0.0-alpha.1")
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+MESSAGE_BACKLOG = {}
+BACKLOG_LENGTH = 100
+PRINT_LIMIT = 10
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat.id in MESSAGE_BACKLOG:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="I'm already listening to this chat."
+        )
+        return
+
+    backlog_length = BACKLOG_LENGTH
+    if context.args:
+        backlog_length = int(context.args[0])
+
+    MESSAGE_BACKLOG[update.effective_chat.id] = Queue(maxsize=backlog_length)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I will now start listening to this chat. "
+        + f"I will save the last {backlog_length} "
+        + "messages and summarize them to you if you ask me to.",
+    )
+
+    logging.info(
+        f"Started listening to <{update.effective_chat.title}> "
+        + f"with id {update.effective_chat.id} "
+        + f"and backlog length {backlog_length}"
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    MESSAGE_BACKLOG.pop(update.effective_chat.id)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I will no longer listen to this chat."
+    )
+
+    logging.info(
+        f"Stopped listening to <{update.effective_chat.title}> "
+        + f"with id {update.effective_chat.id}"
+    )
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+async def show_backlog(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    backlog = MESSAGE_BACKLOG[update.effective_chat.id]
+    if backlog.empty():
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="I haven't seen any messages yet."
+        )
+    elif backlog.qsize() < PRINT_LIMIT:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Here's what I've seen so far:\n{format_backlog(backlog)}",
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Here's what I've seen so far:\n...\n{format_backlog(backlog[-PRINT_LIMIT:])}",
+        )
+
+    logging.info(
+        f"Sent backlog to {update.effective_chat.title} "
+        + f"with id {update.effective_chat.id}"
+    )
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    with open('TOKEN.token') as f:
-        token = f.readlines()[0]
-    application = Application.builder().token(token).build()
+async def log(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    backlog = MESSAGE_BACKLOG[update.effective_chat.id]
+    if backlog.full():
+        backlog.get()
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    user = (
+        update.effective_message.forward_from.name
+        if update.effective_message.forward_from is not None
+        else update.effective_user
+    )
+    backlog.put((user, update.effective_message.text))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    logging.info(
+        f"Added message to backlog of {update.effective_chat.title} "
+        + f"with id {update.effective_chat.id}"
+    )
 
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+
+async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    MESSAGE_BACKLOG[update.effective_chat.id].queue.clear()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Cleared backlog."
+    )
+    logging.info(
+        f"Cleared backlog of {update.effective_chat.title}"
+        + f"with id {update.effective_chat.id}"
+    )
+
+
+async def summarize(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    backlog = MESSAGE_BACKLOG[update.effective_chat.id]
+    logging.info(
+        f"Summarizing {update.effective_chat.title}"
+        + f"with id {update.effective_chat.id}"
+    )
+
+    if backlog.empty():
+        await context.bot.send_message(
+            chat_id=update.effective_user.id, text="I haven't seen any messages yet."
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_user.id, text="Generating summary..."
+        )
+
+        input_text = format_backlog(backlog)
+        summary = summarizer(input_text, max_length=142, min_length=20, do_sample=False)
+
+        await context.bot.send_message(
+            chat_id=update.effective_user.id,
+            text=f"Here is the summary of the last 100 messages in {update.effective_chat.title}:\n"
+            + f"{summary[0]['summary_text']}",  # type: ignore
+        )
+
+    logging.info(
+        f"Sent summary to <{update.effective_user.name}> "
+        + f"with id {update.effective_user.id}"
+    )
+
+
+async def catch_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(
+        f"Received message from {update.effective_chat.title} "
+        + f"with id {update.effective_chat.id}, "
+        + f"sent by {update.effective_user.name} "
+        + f"with id {update.effective_user.id}"
+    )
+
+
+def format_backlog(backlog: Queue):
+    return "\n".join(
+        [f"{name.name}: {message}" for name, message in list(backlog.queue)]
+    )
+
+
+class ListeningTo(filters.MessageFilter):
+    def filter(self, message: Message):
+        return message.chat_id in MESSAGE_BACKLOG
+
+
+listening_to_filter = ListeningTo()
 
 
 if __name__ == "__main__":
-    main()
+    with open("TOKEN.token") as f:
+        token = f.readlines()[0]
+    application = ApplicationBuilder().token(token).build()
+
+    handlers = [
+        CommandHandler("start", start),
+        CommandHandler("stop", stop, filters=listening_to_filter),
+        CommandHandler("backlog", show_backlog, filters=listening_to_filter),
+        CommandHandler("summarize", summarize, filters=listening_to_filter),
+        CommandHandler("clear", clear, filters=listening_to_filter),
+        MessageHandler(filters.TEXT & ~(filters.COMMAND) & listening_to_filter, log),
+        MessageHandler(filters.ALL, catch_all)
+    ]
+
+    application.add_handlers(handlers)
+
+    application.run_polling()
