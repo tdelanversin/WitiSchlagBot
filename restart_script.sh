d56366d0c2d3cea9c128c2fa92c2a8e1261eed9c
Tilman de Lanversin <tilmand@student.ethz.ch>
2023-06-03 17:22:57 +0200

    🚧 wip restarting script

---
+++ restart_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the path to the Python script
+SCRIPT_PATH="/home/pi/repos/WitiSchlagBot/src/WitiBot.py"
+
+# Go to the git directory
+cd "$(git rev-parse --show-toplevel)"
+
+# Pull the latest changes
+git pull origin
+
+# Check if there were any changes
+if [ "$(git diff --name-only origin/master)" ]; then
+    echo "Changes detected. Restarting the Python script..."
+    
+    # Stop the currently running Python script if it's already running
+    pkill -f "$SCRIPT_PATH"
+    
+    # Start the Python script
+    python3 "$SCRIPT_PATH" &
+    
+    echo "Python script restarted."
+else
+    echo "No changes detected. Python script will not be restarted."
+fi
